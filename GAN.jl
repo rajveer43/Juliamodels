ec5b25485fc496b804ef8a8aac4a267498ec6b1c
Rockerz <64583161+rajveer43@users.noreply.github.com>
2023-09-14 15:18:30 +0530

    Update GAN.jl

--- GAN.jl
+++ GAN.jl
@@ -9,3 +9,67 @@ function build_generator(latent_dim, output_dim)
         Dense(256, output_dim, sigmoid)
     )
 end
+
+# Discriminator network
+function build_discriminator(input_dim)
+    return Chain(
+        Dense(input_dim, 256, leakyrelu),
+        Dense(256, 128, leakyrelu),
+        Dense(128, 1, sigmoid)
+    )
+end
+
+# GAN model
+function build_gan(generator, discriminator)
+    return Chain(generator, discriminator)
+end
+
+# Loss functions
+function generator_loss(fake_output)
+    return -mean(log(fake_output))
+end
+
+function discriminator_loss(real_output, fake_output)
+    real_loss = -mean(log(real_output))
+    fake_loss = -mean(log(1.0 - fake_output))
+    return real_loss + fake_loss
+end
+
+# Define hyperparameters
+latent_dim = 100
+input_dim = 784
+output_dim = 784
+learning_rate = 0.0002
+batch_size = 64
+epochs = 10000
+
+# Build and initialize the networks
+generator = build_generator(latent_dim, output_dim)
+discriminator = build_discriminator(input_dim)
+gan = build_gan(generator, discriminator)
+
+# Define optimizers
+optimizer_g = ADAM(learning_rate)
+optimizer_d = ADAM(learning_rate)
+
+# Training loop
+for epoch in 1:epochs
+    for batch in 1:batch_size:60000  # Assuming you have a dataset with 60,000 samples
+        # Train the discriminator
+        real_data = rand(reshape(MNIST.images()[batch:batch+batch_size-1], :, 784), 1)
+        fake_data = generator(randn(latent_dim, batch_size))
+
+        Flux.back!(discriminator_loss(discriminator(real_data), discriminator(fake_data)))
+        Flux.update!(optimizer_d, discriminator)
+        
+        # Train the generator
+        fake_data = generator(randn(latent_dim, batch_size))
+
+        Flux.back!(generator_loss(discriminator(fake_data)))
+        Flux.update!(optimizer_g, generator)
+    end
+
+    if epoch % 100 == 0
+        println("Epoch: $epoch")
+    end
+end
